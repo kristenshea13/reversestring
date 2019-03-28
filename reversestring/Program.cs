53b3571802f39454a1e119ebfc5d55b56f5a071a
Kristen Shea <kstar61@live.com>
2019-03-28 17:43:50 -0500

    /

--- reversestring/Program.cs
+++ reversestring/Program.cs
@@ -6,7 +6,17 @@ namespace reversestring
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string hello = "Hello World!";
+
+
+            var helloWorld = hello.ToCharArray();
+
+            Array.Reverse(helloWorld);
+
+            string reverseStr = new string(helloWorld);
+
+
+
         }
     }
 }
